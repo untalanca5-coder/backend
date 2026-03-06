954ca797629d443b6b7231a9d016b1c3ae696eed
untalanca5-coder <untalanca5@gmail.com>
2026-03-06 12:35:32 +0800

    Refactor server.js to simplify middleware
    Removed logger middleware and moment dependency, cleaned up code.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //API framework
-
-const express = require('express');
+const express=require('express');
 //cross origin resource sharing
 const cors=require('cors');
 //Environment variables
@@ -10,30 +9,23 @@ const db=require('./config/db');
 //Routes
 const routes=require('./routes');
 
-//utilization of express
-const app = express();
-
+const app=express();
 //moment
-const moment = require('moment');
-//Logger middleware
-const logger=(req,res,next)=>{
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} - ${moment().format()}`);
-    next();
-};
-app.use(logger);
+// const moment = require('moment');
+// const logger = (req, res, next) => {
+//     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} - ${moment().format()}`);
+//     next();
+// };
 
-
-
-
+// app.use(logger);
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));//this will allow us to read the url body tags
+app.use(express.urlencoded({extended:true}));//this will allow us to read the URL body tags
 
 //Use routes
-app.use('/api',routes)
+app.use('/api',routes);
 
 //Server listening
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on port ${process.env.PORT}`);
 });
-
